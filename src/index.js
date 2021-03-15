5305645d1dd7057474d57e6a8784a7a5eb02c74f
Artem <drta26@gmail.com>
2021-03-15 22:18:27 +0300

    feat adding new code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if(Array.isArray(array)) {
+    let result = array[0];
+    array.forEach((item) => {
+       result = item < result ? item : result;});
+  return result;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array.length !==0) {
+    let result = array[0];
+    array.forEach((item) => {
+       result = item > result ? item : result;});
+  return result;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if(array.length != 0) {
+    let sum = 0;
+    array.forEach((item) => {
+       sum += item});
+   result = sum / array.length;}
+  return result;
 }
